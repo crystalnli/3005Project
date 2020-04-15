ae95e6fa66c9ad0ce3faa1a7afd2ee236049f018
Crystal <licrystal90@gmail.com>
2020-04-14 22:54:59 -0400

    Update project

--- UniqueBookStore/src/CustomerMainPage.java
+++ UniqueBookStore/src/CustomerMainPage.java
@@ -3,50 +3,122 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class CustomerMainPage extends JFrame{
 
 	public CustomerMainPage() {
 		JFrame cMainPage = new JFrame("Book store for customer");
+		JPanel controlPanel = new JPanel();
 		JPanel main = new JPanel();
+		main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
 		JLabel j1 = new JLabel("Welcome to Unique Book Store!");
+		j1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
 		JButton search = new JButton("search");
+		search.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JButton cart = new JButton("cart");
+		cart.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JButton myOrder = new JButton ("my order");
+		myOrder.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JButton check = new JButton ("check order");
+		check.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JButton logout = new JButton("log out");
-		
-		JButton ret = new JButton("return");
-		ret.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				main.setVisible(true);
-			}
-		});
+		logout.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
 		
+		
+		main.add(j1);
 		main.add(search);
 		main.add(cart);
 		main.add(myOrder);
 		main.add(check);
 		main.add(logout);
 		
+
 		
 		
 		JPanel searchPage = new JPanel();
-		JLabel j2 = new JLabel("search by");
-		searchPage.add(ret);
-
+		searchPage.setLayout(new BoxLayout(searchPage, BoxLayout.Y_AXIS));
+		JTextField value = new JTextField();
+		value.setAlignmentX(Component.CENTER_ALIGNMENT);
+		JLabel j2 = new JLabel("search book by");
+		j2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		String[] option = {"name","author","ISBN","genre"};
+		JList list = new JList(option);
+		list.setAlignmentX(Component.CENTER_ALIGNMENT);
+		JButton ok = new JButton("OK");
+		ok.setAlignmentX(Component.CENTER_ALIGNMENT);
+		ok.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				String key = option.toString();
+				
+			}
+		});
 		
-		cMainPage.add(main);
-		//cMainPage.add(searchPage);
-		searchPage.setVisible(false);
+		JButton ret = new JButton("return");
+		ret.setAlignmentX(Component.CENTER_ALIGNMENT);
+		ret.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				controlPanel.remove(searchPage);
+				controlPanel.add(main);
+				controlPanel.revalidate();
+				controlPanel.repaint();
+			}
+		});
+		
+		searchPage.add(value);
+		searchPage.add(j2);
+		searchPage.add(list);
+		searchPage.add(ok);
+		searchPage.add(ret);
+		
+		search.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				controlPanel.remove(main);
+				controlPanel.add(searchPage);
+				controlPanel.revalidate();
+				controlPanel.repaint();
+				
+			}
+		});
+		controlPanel.add(main);
+		cMainPage.add(controlPanel);
 		
 		cMainPage.setBounds(500,500,500,500);
 		cMainPage.setVisible(true);
 		cMainPage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
+	}
+	
+	public ArrayList<String> sqlSearch(String key,String value) {
+		ArrayList<String> books = new ArrayList<String>();
+		try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BookStore",  "postgres", "Crystal2013");
+	            Statement statement = connection.createStatement();
+				)
+				{
+				try {
+					String state = "(select * from book where " + key +" = '" + value + "')";
+					ResultSet rs = statement.executeQuery(state);
+                    while (rs.next()) {
+                           System.out.println(rs.getString(1));
+                           books.add(rs.getString(1));
+                        
+                    }
+					return books;
+					
+				} catch (Exception sqle) {
+		            System.out.println("Exception: " + sqle);
+		        }
+	            
+
+	    
+	        } catch (Exception sqle) {
+	            System.out.println("Exception: " + sqle);
+	        }
+		return books;
 	}
 	
 	 public static void main(String[] args){
